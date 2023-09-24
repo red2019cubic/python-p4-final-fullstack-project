1609af277c70f5492285957cdd20b6a3a1da2dd6
Red <red.sarouf2014@gmail.com>
2023-09-24 09:43:56 -0400

    create all models

--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
-from datetime import dt
+from datetime import datetime
 
 from config import db
 
@@ -10,17 +10,33 @@ from config import db
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
 
-from config import db, bcrypt
+from config import db
+import bcrypt
+
+employee_task = db.Table('employee_task',
+    db.Column('employee_id', db.Integer, db.ForeignKey('employee.id')),
+    db.Column('task_id', db.Integer, db.ForeignKey('task.id'))
+)
+
+# Define the Employee-Department association table
+employee_department = db.Table('employee_department',
+    db.Column('employee_id', db.Integer, db.ForeignKey('employee.id')),
+    db.Column('department_id', db.Integer, db.ForeignKey('department.id'))
+)
 
 class Employee(db.Model, SerializerMixin):
-    __tablename__ = "employees"
+    
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     username = db.Column(db.String, unique=True)
-    clocked_in = db.Column(db.DateTime, default=dt.utcnow())
-    clocked_out = db.Column(db.DateTime, default=dt.utcnow())
+    admin = db.Column(db.String, default=False)
+    clocked_in = db.Column(db.DateTime, server_default=db.func.now())
+    clocked_out = db.Column(db.DateTime, onupdate=db.func.now())
     _password_hash = db.Column(db.String)
+    
+    tasks = db.relationship('Task',secondary=employee_task, backref=db.backref('employees', lazy='dynamic'))
+    departments = db.relationship('Department', secondary=employee_department, backref=db.backref('employees', lazy='dynamic'))
 
     @hybrid_property # Restrict access to the password hash.
     def password_hash(self):
@@ -28,7 +44,7 @@ class Employee(db.Model, SerializerMixin):
 
     @password_hash.setter # Generate a Bcrypt password hash and set it to the _password_hash attribute
     def password_hash(self, password):
-        bcrypt_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+        bcrypt_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
         self._password_hash = bcrypt_hash
 
     def authenticate(self, password): # Check if the provided password matches the one stored in the db
@@ -36,24 +52,31 @@ class Employee(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f"Employee {self.username}, ID: {self.id}, Name:{self.name}, Clocked_IN:{self.clocked_in}, Clocked_OUT:{self.clocked_out}"
+    
 class Task(db.Model, SerializerMixin):
-    __tablename__ = "tasks"
+ 
 
     id = db.Column(db.Integer, primary_key=True)
-    task_title = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    
 
 
     def __repr__(self):
-        return f"Task {self.task_title}, ID: {self.id}"
+        return f"Task {self.task_name}, ID: {self.id}"
 
-class Departments(db.Model, SerializerMixin):
-    __tablename__ = "departments"
+
+
+
+class Department(db.Model, SerializerMixin):
+    
 
     id = db.Column(db.Integer, primary_key=True)
-    dept_title = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+
+
 
 
     def __repr__(self):
-        return f"Departments {self.dept_title}, ID: {self.id}"
+        return f"Department {self.dept_title}, ID: {self.id}"
 
 
--- server/seed.py
+++ server/seed.py
@@ -6,12 +6,47 @@ from random import randint, choice as rc
 # Remote library imports
 from faker import Faker
 
+
+
 # Local imports
 from app import app
-from models import db
+from config import db
+from models import Employee, Department, Task, employee_department, employee_task  # Import your SQLAlchemy models
 
-if __name__ == '__main__':
-    fake = Faker()
-    with app.app_context():
-        print("Starting seed...")
-        # Seed code goes here!
+fake = Faker()
+with app.app_context():
+    # Employee.query.delete()
+    # Task.query.delete()
+    # Department.query.delete()
+    db.create_all()
+    print("Starting seed...")
+        
+        
+
+        
+        
+
+# Create sample data for employees, departments, and tasks
+    employees_data = [
+            {"name": fake.name(),"username": fake.email(), "password":fake.name(), "admin": True, "tasks": ["ICQA Primary", "In-Bound SOUTH"], "departments": ["ICQA", "INDUCT"]},
+            {"name": fake.name(),"username": fake.email(), "password":fake.name(), "admin": False,"tasks": ["ICQA SouthWest", "Task 3"], "departments": ["Department 2"]},
+            {"name": fake.name(),"username": fake.email(), "password":fake.name(), "admin": True, "tasks": ["ICQA South", "ICQA North"], "departments": ["ICQA", "In-Bound", "Out-Bound"]},
+        ]
+    for emp_data in employees_data:
+        employee = Employee(name=emp_data["name"], username=emp_data["username"],  admin=True)
+        employee.password_hash = emp_data["name"]
+        for task_name in emp_data["tasks"]:
+            task = Task.query.filter_by(name=task_name).first()
+            if not task:
+                task = Task(name=task_name)
+                db.session.add(task)
+                employee.tasks.append(task)
+            for dept_name in emp_data["departments"]:
+                department = Department.query.filter_by(name=dept_name).first()
+                if not department:
+                    department = Department(name=dept_name)
+                    db.session.add(department)
+                    employee.departments.append(department)
+            db.session.add(employee)
+        db.session.commit()
+    print("Saving complete...")
